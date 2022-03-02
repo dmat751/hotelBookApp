01611ad832ea5370fa5b9447a19d81b42f55de3e
Dawid Matras <46224840+dmat751@users.noreply.github.com>
2022-03-02 15:58:59 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
-import classes from './App.module.scss';
 import Hero from './components/Hero/Hero';
 import HotelList from './components/HotelList/HotelList';
-import { Fragment, useEffect, useState } from 'react';
+import { Fragment } from 'react';
 import { useSelector } from 'react-redux';
 import { selectHotelList } from './store';
-
-function getRandomInt(min: number, max: number) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}
 
 function App() {
   const hotelListItem = useSelector(selectHotelList);
 
-  let heroUrl;
-  let heroAlt;
+  let heroUrl = '';
+  let heroAlt = '';
 
   try {
     heroUrl = hotelListItem.hotelList[0].images[0].url;
@@ -27,7 +20,7 @@ function App() {
 
   return (
     <Fragment>
-      <Hero heroPhotoUrl={heroUrl} heroPhotoAlt={''}></Hero>
+      <Hero heroPhotoUrl={heroUrl} heroPhotoAlt={heroAlt}></Hero>
       <HotelList></HotelList>
     </Fragment>
   );
