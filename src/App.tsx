9b0db51186b7cce36cde352016ad917ea7721e10
Dawid Matras <46224840+dmat751@users.noreply.github.com>
2022-03-02 19:18:18 +0100

    add hero random photo

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,52 @@ import HotelList from './components/HotelList/HotelList';
 import { Fragment } from 'react';
 import { useSelector } from 'react-redux';
 import { selectHotelList } from './store';
+import { Hotel } from './models/Hotel';
+
+interface Photo {
+  url: string;
+  alt: string;
+}
+
+const getRandomInt = (min: number, max: number): number => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+const selectRandonPhoto = (hotelListItem: Hotel[]): Photo => {
+  let imgArray: Photo[] = [];
+  hotelListItem.forEach((hotelItem) => {
+    hotelItem.images.forEach((imgItem) => {
+      imgArray.push(imgItem);
+    });
+  });
+  let randomImgArrayIndex = getRandomInt(0, imgArray.length);
+  if (imgArray[randomImgArrayIndex]) {
+    return imgArray[randomImgArrayIndex];
+  } else {
+    const result: Photo = { alt: '', url: '' };
+    return result;
+  }
+};
 
 function App() {
   const hotelListItem = useSelector(selectHotelList);
 
-  let heroUrl = '';
-  let heroAlt = '';
-
+  let randomPhoto: Photo = { alt: '', url: '' };
   try {
-    heroUrl = hotelListItem.hotelList[0].images[0].url;
-    heroAlt = hotelListItem.hotelList[0].images[0].alt;
+    randomPhoto = selectRandonPhoto(hotelListItem.hotelList);
   } catch (error) {
-    heroUrl = '';
-    heroAlt = '';
+    randomPhoto.alt = '';
+    randomPhoto.url = '';
   }
-
-  console.log('test app');
 
   return (
     <Fragment>
-      <Hero heroPhotoUrl={heroUrl} heroPhotoAlt={heroAlt}></Hero>
+      <Hero
+        heroPhotoUrl={randomPhoto.url}
+        heroPhotoAlt={randomPhoto.alt}
+      ></Hero>
       <HotelList></HotelList>
     </Fragment>
   );
