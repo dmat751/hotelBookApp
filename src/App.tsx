e1236cf7d4e0a869eb4eff3ba1d4c8b82225f9c4
Dawid Matras <46224840+dmat751@users.noreply.github.com>
2022-03-04 11:05:50 +0100

    refactoring v1

--- src/App.tsx
+++ src/App.tsx
@@ -2,45 +2,12 @@ import Hero from './components/Hero/Hero';
 import HotelList from './components/HotelList/HotelList';
 import { useSelector } from 'react-redux';
 import { selectHotelList } from './store';
-import { Hotel } from './models/Hotel';
-
-interface Photo {
-  url: string;
-  alt: string;
-}
-
-const getRandomInt = (min: number, max: number): number => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-};
-
-const selectRandomPhoto = (hotelListItem: Hotel[]): Photo => {
-  let imgArray: Photo[] = [];
-  hotelListItem.forEach((hotelItem) => {
-    hotelItem.images.forEach((imgItem) => {
-      imgArray.push(imgItem);
-    });
-  });
-  let randomImgArrayIndex = getRandomInt(0, imgArray.length);
-  if (imgArray[randomImgArrayIndex]) {
-    return imgArray[randomImgArrayIndex];
-  } else {
-    const result: Photo = { alt: '', url: '' };
-    return result;
-  }
-};
+import { Photo, selectRandomPhoto } from './helpers/Helper';
 
 function App() {
   const hotelListItem = useSelector(selectHotelList);
 
-  let randomPhoto: Photo = { alt: '', url: '' };
-  try {
-    randomPhoto = selectRandomPhoto(hotelListItem.hotelList);
-  } catch (error) {
-    randomPhoto.alt = '';
-    randomPhoto.url = '';
-  }
+  const randomPhoto = selectRandomPhoto(hotelListItem.hotelList);
 
   return (
     <>
