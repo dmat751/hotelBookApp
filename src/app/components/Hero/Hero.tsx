43e979fdb69aa6b472386c6c872f652474ee29ad
Dawid Matras <46224840+dmat751@users.noreply.github.com>
2022-08-10 15:02:30 +0200

    fix components params

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -1,17 +1,16 @@
 import FormFilter from './HeroFilter/HeroFilter';
 import classes from './Hero.module.scss';
 
-const Hero: React.FC<{ heroPhotoUrl: string; heroPhotoAlt: string }> = (
-  props
-) => {
+type Props = Readonly<{heroPhotoUrl: string; heroPhotoAlt: string}>
+const Hero = ({heroPhotoUrl, heroPhotoAlt} : Props) => {
   return (
     <div className={classes.hero}>
       <div className={classes['hero__img-container']}>
-        {props.heroPhotoUrl && (
+        {heroPhotoUrl && (
           <img
             className={classes.hero__img}
-            src={props.heroPhotoUrl}
-            alt={props.heroPhotoAlt}
+            src={heroPhotoUrl}
+            alt={heroPhotoAlt}
           />
         )}
       </div>
