b5e884b95e1cb8fc1635350557f55115e421f015
Dawid Matras <46224840+dmat751@users.noreply.github.com>
2022-08-13 11:14:27 +0200

    refactor hero & app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,10 @@
-import { useSelector } from 'react-redux';
-import Hero from './components/Hero/Hero';
+import { Hero } from './components/Hero/Hero';
 import HotelList from './components/HotelList/HotelList';
-import { selectHotelList } from './store';
-import { selectRandomPhoto } from '../helpers/Helper';
 
 function App() {
-  const hotelListItem = useSelector(selectHotelList);
-
-  const randomPhoto = selectRandomPhoto(hotelListItem.hotelList);
-
   return (
     <>
-      <Hero
-        heroPhotoUrl={randomPhoto.url}
-        heroPhotoAlt={randomPhoto.alt}
-      ></Hero>
+      <Hero />
       <HotelList />
     </>
   );
--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -1,16 +1,21 @@
 import FormFilter from './HeroFilter/HeroFilter';
 import classes from './Hero.module.scss';
+import { useSelector } from 'react-redux';
+import { selectHotelList } from '../../store';
+import { selectRandomPhoto } from '../../../helpers/Helper';
 
-type Props = Readonly<{heroPhotoUrl: string; heroPhotoAlt: string}>
-const Hero = ({heroPhotoUrl, heroPhotoAlt} : Props) => {
+export const Hero = () => {
+  const hotelListItem = useSelector(selectHotelList);
+
+  const randomPhoto = selectRandomPhoto(hotelListItem.hotelList);
   return (
     <div className={classes.hero}>
       <div className={classes['hero__img-container']}>
-        {heroPhotoUrl && (
+        {randomPhoto.url && (
           <img
             className={classes.hero__img}
-            src={heroPhotoUrl}
-            alt={heroPhotoAlt}
+            src={randomPhoto.url}
+            alt={randomPhoto.alt}
           />
         )}
       </div>
@@ -18,4 +23,3 @@ const Hero = ({heroPhotoUrl, heroPhotoAlt} : Props) => {
     </div>
   );
 };
-export default Hero;
