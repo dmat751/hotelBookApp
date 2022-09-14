da93235a4e6aaf49fcb10fe8d91ae10ecea089ef
Dawid Matras <46224840+dmat751@users.noreply.github.com>
2022-09-14 16:54:36 +0200

    remove ApiQueryStatus

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { hotelListSlice } from '../../modules/hotelList/hotelListSlice';
 import { hotelFiltersSlice } from '../../modules/hotelFilters/hotelFiltersSlice';
-import { ApiQueryStatusSlice } from '../../modules/apiStatus/ApiStatusSlice';
 
 export const store = configureStore({
   reducer: {
     hotelList: hotelListSlice.reducer,
     hotelFilters: hotelFiltersSlice.reducer,
-    apiQueryStatus: ApiQueryStatusSlice.reducer,
   },
 });
--- src/modules/apiStatus/ApiStatusSelector.ts
+++
@@ -1,3 +0,0 @@
-import {RootState} from "../../app/types/rootState";
-
-export const selectApiQueryStatus = (state: RootState) => state.apiQueryStatus;
