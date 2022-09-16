ad1f8747aa42fc85834f5df361c2a6a16cb9ffca
Dawid Matras <46224840+dmat751@users.noreply.github.com>
2022-09-16 10:35:01 +0200

    Update getMaxHotelValueByProp.ts

--- src/app/queries/getMaxHotelValueByProp.ts
+++ src/app/queries/getMaxHotelValueByProp.ts
@@ -4,15 +4,11 @@ export const getMaxHotelValueByProp = (
   hotelList: Hotel[],
   prop: 'maxChildren' | 'maxAdults'
 ) => {
-  try {
-    const childrenMaxes = hotelList
-      .map((hotel) =>
-        hotel.roomsDetails.rooms.map((room) => room?.occupancy?.[prop] ?? 0)
-      )
-      .flat();
+  const childrenMaxes = hotelList
+    .map((hotel) =>
+      hotel.roomsDetails.rooms.map((room) => room?.occupancy?.[prop] ?? 0)
+    )
+    .flat();
 
-    return childrenMaxes.length > 0 ? Math.max(...childrenMaxes) : 0;
-  } catch (error) {
-    return 0;
-  }
+  return Math.max(...childrenMaxes) || 0;
 };
