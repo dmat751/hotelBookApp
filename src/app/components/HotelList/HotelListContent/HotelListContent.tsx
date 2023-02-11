f1acf788aff62f8b2c0c560ef6624766c6ced81d
Dawid Matras <46224840+dmat751@users.noreply.github.com>
2023-02-11 14:28:52 +0100

    RTQ Q done

--- src/app/components/HotelList/HotelListContent/HotelListContent.tsx
+++ src/app/components/HotelList/HotelListContent/HotelListContent.tsx
@@ -2,9 +2,12 @@ import { useSelector } from 'react-redux';
 import { selectFilteredHotelList } from '../../../../modules/hotelList/filteredHotelListSelector';
 import { HotelItem } from '../HotelItem/HotelItem';
 import baseClasses from '../../../assets/baseClasses.module.scss';
+import { selectFilteredHotelListApi } from '../../../../modules/hotelList/api/selector';
 
 export const HotelListContent = () => {
-  const hotelListItems = useSelector(selectFilteredHotelList);
+  // const hotelListItems = useSelector(selectFilteredHotelList);
+  const hotelListItems = useSelector(selectFilteredHotelListApi);
+  // console.log(hotelListItems2);
 
   return (
     <ul className={baseClasses['basic-container-1']}>
