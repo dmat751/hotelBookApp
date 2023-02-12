df6eb1f7885dc3d4ce6460442cc5325d3e79fd8e
Dawid Matras <46224840+dmat751@users.noreply.github.com>
2023-02-12 15:12:16 +0100

    add randomHotelPhotoSelector v2

---
+++ src/modules/hotelList/selectors/test/randomHotelPhotoSelector.test.tsx
@@ -0,0 +1,60 @@
+import { screen } from '@testing-library/react';
+import { App } from '../../../../app/App';
+import { renderWithProviders } from '../../../../app/utils/test-utils';
+import { Photo } from '../../types/room';
+
+describe('test randomHotelPhotoSelector', () => {
+  //given
+  type TestCase = {
+    expectedResult: Photo;
+    getRandomNumberMockedValue: number;
+  };
+
+  const cases: TestCase[] = [
+    {
+      expectedResult: {
+        alt: '',
+        url: 'https://rl-uk2.azureedge.net/picturemanager/images/OBMNG1/Hotel1.JPG',
+      },
+      getRandomNumberMockedValue: 0,
+    },
+    {
+      expectedResult: {
+        alt: '',
+        url: 'https://rl-uk2.azureedge.net/picturemanager/images/OBMNG1/hotel4.jpg',
+      },
+      getRandomNumberMockedValue: 1,
+    },
+  ].map((caseItem) =>
+    Object.assign(caseItem, {
+      toString: () => {
+        return `
+        getRandomNumberMockedValue: ${caseItem.getRandomNumberMockedValue}
+        expected result: {url: ${caseItem.expectedResult.url}, alt: ${caseItem.expectedResult.alt}}`;
+      },
+    } as TestCase)
+  );
+
+  test.each<TestCase>(cases)(
+    'test for %s',
+    async ({ getRandomNumberMockedValue, expectedResult }) => {
+      //when
+      const getRandomNumber = require('../../../../app/utils/getRandomNumber');
+      const mock = jest
+        .spyOn(getRandomNumber, 'getRandomNumber')
+        .mockReturnValue(getRandomNumberMockedValue);
+
+      await renderWithProviders(<App />, {
+        preloadedState: {
+          hotelFilters: { adults: 0, children: 0, stars: 1 },
+        },
+      });
+
+      //then
+      const foundItem = screen.findByTestId(expectedResult.url);
+      expect(await foundItem).toBeInTheDocument();
+
+      mock.mockRestore();
+    }
+  );
+});
