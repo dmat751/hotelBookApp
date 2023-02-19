51ba21d72b2111da2295eea093bf1693c5678b85
Dawid Matras <46224840+dmat751@users.noreply.github.com>
2023-02-19 14:33:46 +0100

    add absolute path

---
+++ craco.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.paths.json');
+module.exports = {
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  //   jest: {
+  //     preset: 'ts-jest',
+  //     alias: {
+  //       '@': path.resolve(__dirname, 'src'),
+  //     },
+  //   },
+  jest: {
+    configure: {
+      preset: 'ts-jest',
+      moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+        prefix: '<rootDir>/',
+      }),
+    },
+  },
+};
