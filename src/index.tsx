8e8cc452433562a7fec20431e10de1c80918dc34
Dawid Matras <46224840+dmat751@users.noreply.github.com>
2023-02-19 15:40:23 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import { store } from '@/app/store/store';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import '@/index.scss';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
