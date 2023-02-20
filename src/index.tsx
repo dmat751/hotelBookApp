1b43e57927cc4377b5ad800b412b7140012a7661
Dawid Matras <46224840+dmat751@users.noreply.github.com>
2023-02-20 10:22:00 +0100

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.REACT_APP_NODE_ENV === 'development') {
   const { worker } = require('./mocks/browser');
   worker.start();
 }
