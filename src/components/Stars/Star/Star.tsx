7952f8f11d88be810efcf165808ab32e7675516e
Dawid Matras <46224840+dmat751@users.noreply.github.com>
2023-02-21 11:51:43 +0100

    refactor

--- src/components/Stars/Star/Star.tsx
+++ src/components/Stars/Star/Star.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames';
+import { memo } from 'react';
 
 type Props = Readonly<{
   borderColor: string;
@@ -7,30 +8,27 @@ type Props = Readonly<{
   onClickHandler?: () => void;
 }>;
 
-export const Star = ({
-  borderColor,
-  fillColor,
-  dataTestId,
-  onClickHandler,
-}: Props) => (
-  <div onClick={onClickHandler} data-testid={dataTestId}>
-    <svg
-      className={classNames('m-[2px] h-[30px] w-[30px]', {
-        'cursor-pointer': onClickHandler,
-      })}
-      viewBox="0 0 300 275"
-      xmlns="http://www.w3.org/2000/svg"
-      version="1.1"
-    >
-      <polygon
-        data-testid={`star-polygon-${fillColor}`}
-        fill={fillColor}
-        stroke={borderColor}
-        strokeWidth="15"
-        points="150,25  179,111 269,111 197,165
+export const Star = memo(
+  ({ borderColor, fillColor, dataTestId, onClickHandler }: Props) => (
+    <div onClick={onClickHandler} data-testid={dataTestId}>
+      <svg
+        className={classNames('m-[2px] h-[30px] w-[30px]', {
+          'cursor-pointer': onClickHandler,
+        })}
+        viewBox="0 0 300 275"
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+      >
+        <polygon
+          data-testid={`star-polygon-${fillColor}`}
+          fill={fillColor}
+          stroke={borderColor}
+          strokeWidth="15"
+          points="150,25  179,111 269,111 197,165
                     223,251  150,200 77,251  103,165
                     31,111 121,111"
-      />
-    </svg>
-  </div>
+        />
+      </svg>
+    </div>
+  )
 );
